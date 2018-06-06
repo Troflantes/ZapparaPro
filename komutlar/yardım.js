44a41d1ec33f396d32b6f6473870a329b616e0d3
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-06 15:44:21 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -6,12 +6,11 @@ module.exports.run = async (bot, message, args) => {
     .setAuthor("85 Tane Komut Var", bot.user.displayAvatarURL)
     .addField("Moderasyon", "`" + message.prefix + "moderasyon`", true)
     .addField("Oyunlar", "`" + message.prefix + "oyunlar`", true)
-    .addField("Bayraklar", "`" + message.prefix + "bayrak`", true)
-    .addField("Müzik", "`" + message.prefix + "Müzik`", true)
+    .addField("Müzik", "`" + message.prefix + "müzik`", true)
     .addField("Eğlence", "`" + message.prefix + "eğlence`", true)
     .addField("İstatistikler", "`" + message.prefix + "istatistikler`", true)
     .addField("Admin", "`" + message.prefix + "admin`", true)
-  .setFooter("Gördüğünüz tüm komutlar JavaScript'tir -" + message.prefix + "yardım" + "- Botun Sahibi: Enes Onur Ata#9427")
+  .setFooter("Gördüğünüz tüm komutlar JavaScript'tir » " + message.prefix + "yardım" + " » Botun Sahibi: Enes Onur Ata#9427")
     message.channel.send(embed);
 }
 module.exports.help = {
