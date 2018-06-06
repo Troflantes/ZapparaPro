bac083067a3453b16d9f0790995016c6a69bcacc
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-06-06 15:42:59 +0300

    Update oyunlar.js

--- komutlar/oyunlar.js
+++ komutlar/oyunlar.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
 
     let embed = new Discord.RichEmbed()
-    .setTitle("<:TAfun:444855091585875979> OYUNLAR")
+    .setTitle(":video_game:  OYUNLAR")
     .addField("`" + message.prefix + "fortnite`", "Kullanıcının Fortnite istatistiklerine bak!")
     .addField("`" + message.prefix +"csgo`", "Kullanıcının CSGO istatistiklerine bak!");
     message.channel.send(embed);
